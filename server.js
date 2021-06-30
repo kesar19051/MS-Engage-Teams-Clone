32a77db2a3a1ad5b62512536e95dc70ec31398e7
Kesar Shrivastava <kesar19051@iiitd.ac.in>
2021-06-30 21:07:39 +0530

    chat and video call working together but video call disappears once the chatting option is clicked

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 const express = require('express')
 const app = express()
 const http = require('http').Server(app)
-const io = require('socket.io')(http)
+const io = require('socket.io')(3000)
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + "/public"))
 let clients = 0
 
-io.on('connection', function (socket) {
-    socket.on("NewClient", function () {
+// const io = require('socket.io')(3000)
+
+const users = {}
+
+// io.on('connection', socket => {
+//   socket.on('new-user', name => {
+//     users[socket.id] = name
+//     socket.broadcast.emit('user-connected', name)
+//   })
+//   socket.on('send-chat-message', message => {
+//     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+//   })
+//   socket.on('disconnect', () => {
+//     socket.broadcast.emit('user-disconnected', users[socket.id])
+//     delete users[socket.id]
+//   })
+// })
+
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+    })
+    socket.on("NewClient", () => {
         if (clients < 2) {
             if (clients == 1) {
                 this.emit('CreatePeer')
@@ -40,6 +69,3 @@ function SendAnswer(data) {
 }
 
 http.listen(port, () => console.log(`Active on ${port} port`))
-
-
-
