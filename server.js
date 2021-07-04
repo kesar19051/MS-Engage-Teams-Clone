89566b027eb0d2b126f7bb46be2f59b34180c0f5
Kesar Shrivastava <kesar19051@iiitd.ac.in>
2021-07-04 11:24:44 +0530

    the problem still persists

--- server.js
+++ server.js
@@ -1,31 +1,24 @@
 const express = require('express')
 const app = express()
 const http = require('http').Server(app)
-const io = require('socket.io')(3000)
+const io = require('socket.io')(http)
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + "/public"))
 let clients = 0
-
-// const io = require('socket.io')(3000)
-
 const users = {}
 
-// io.on('connection', socket => {
-//   socket.on('new-user', name => {
-//     users[socket.id] = name
-//     socket.broadcast.emit('user-connected', name)
-//   })
-//   socket.on('send-chat-message', message => {
-//     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-//   })
-//   socket.on('disconnect', () => {
-//     socket.broadcast.emit('user-disconnected', users[socket.id])
-//     delete users[socket.id]
-//   })
-// })
-
-io.on('connection', socket => {
+io.on('connection', function (socket) {
+    socket.on("NewClient", function () {
+        if (clients < 2) {
+            if (clients == 1) {
+                this.emit('CreatePeer')
+            }
+        }
+        else
+            this.emit('SessionActive')
+        clients++;
+    })
     socket.on('new-user', name => {
         users[socket.id] = name
         socket.broadcast.emit('user-connected', name)
@@ -36,16 +29,6 @@ io.on('connection', socket => {
     socket.on('disconnect', () => {
         socket.broadcast.emit('user-disconnected', users[socket.id])
         delete users[socket.id]
-    })
-    socket.on("NewClient", () => {
-        if (clients < 2) {
-            if (clients == 1) {
-                this.emit('CreatePeer')
-            }
-        }
-        else
-            this.emit('SessionActive')
-        clients++;
     })
     socket.on('Offer', SendOffer)
     socket.on('Answer', SendAnswer)
